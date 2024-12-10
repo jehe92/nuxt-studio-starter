85d8334b1e612774e5a87b5b1de3b21ed0bc545f
Jessica Hempel <jessica.hempel@incognito.ms>
2024-12-10 17:58:26 +0100

    feat: adjust deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import appConfig from "./app.config.js";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-12-10",
   devtools: { enabled: true },
@@ -7,6 +10,27 @@ export default defineNuxtConfig({
 
   routeRules: {
     "/": { prerender: true },
+  },
+
+  // Global App config
+  app: {
+    // Global page headers
+    head: {
+      title: "Nuxt Studio Demo",
+      htmlAttrs: {
+        lang: "de",
+      },
+      meta: [
+        { charset: "utf-8" },
+        {
+          name: "viewport",
+          content:
+            "initial-scale=1, maximum-scale=5, minimum-scale=1, width=device-width, height=device-height",
+        },
+      ],
+    },
+    // base config
+    baseURL: appConfig.baseURL,
   },
 
   // Auto import components
